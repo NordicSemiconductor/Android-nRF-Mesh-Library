061c50f53bdeb442ddb5e3be9acdc81e4432a4a2
Roshan Rajaratnam <roshan.rajaratnam@nordicsemi.no>
2018-09-21 14:56:29 +0200

    wip generic level ui

--- Example/nrf-mesh/app/src/main/java/no/nordicsemi/android/nrfmeshprovisioner/GenericLevelServerActivity.java
+++ Example/nrf-mesh/app/src/main/java/no/nordicsemi/android/nrfmeshprovisioner/GenericLevelServerActivity.java
@@ -2,11 +2,14 @@ package no.nordicsemi.android.nrfmeshprovisioner;
 
 import android.arch.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import javax.inject.Inject;
 
 import no.nordicsemi.android.meshprovisioner.configuration.MeshModel;
-import no.nordicsemi.android.meshprovisioner.models.GenericOnOffServerModel;
+import no.nordicsemi.android.meshprovisioner.models.GenericLevelServerModel;
 
 public class GenericLevelServerActivity extends BaseModelConfigurationActivity {
 
@@ -15,8 +18,8 @@ public class GenericLevelServerActivity extends BaseModelConfigurationActivity {
 
     @Override
     protected final void addControlsUi(final MeshModel model) {
-        if(model instanceof GenericOnOffServerModel){
-
+        if(model instanceof GenericLevelServerModel){
+            final CardView cardView = findViewById(R.id.node_controls_card);
         }
     }
 
