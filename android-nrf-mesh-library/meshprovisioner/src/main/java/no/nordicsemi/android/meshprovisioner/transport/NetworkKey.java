60c4040cb50e9407de1a26d74167b4e24299a8c2
Rajaratnam, Roshan <Roshan.Rajaratnam@nordicsemi.no>
2018-10-31 08:07:30 +0100

    added friendly names and key refresh phase fields

--- android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/NetworkKey.java
+++ android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/NetworkKey.java
@@ -2,19 +2,36 @@ package no.nordicsemi.android.meshprovisioner.transport;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.IntDef;
 
 import com.google.gson.annotations.Expose;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Wrapper class for network key
  */
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "unused"})
 public final class NetworkKey implements Parcelable {
 
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({PHASE_0, PHASE_1, PHASE_2})
+    public @interface KeyRefreshPhases {}
+
+    // Key refresh phases
+    public static final int PHASE_0 = 0; //Distribution of new keys
+    public static final int PHASE_1 = 1; //Switching to the new keys
+    public static final int PHASE_2 = 2; //Revoking the old keys
+
+    @Expose
+    private String name = "Network Key";
     @Expose
     final int keyIndex;
     @Expose
     final byte[] key;
+    @Expose
+    private int keyRefreshPhase = PHASE_0;
 
     /**
      * Constructs a NetworkKey object with a given key index and network key
@@ -28,8 +45,23 @@ public final class NetworkKey implements Parcelable {
     }
 
     protected NetworkKey(Parcel in) {
+        name = in.readString();
         keyIndex = in.readInt();
         key = in.createByteArray();
+        keyRefreshPhase = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(keyIndex);
+        dest.writeByteArray(key);
+        dest.writeInt(keyRefreshPhase);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public static final Creator<NetworkKey> CREATOR = new Creator<NetworkKey>() {
@@ -44,22 +76,54 @@ public final class NetworkKey implements Parcelable {
         }
     };
 
+    /**
+     * Returns the friendly name assigned to a network key
+     *
+     * @return String containing the a friendly name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets a friendly name to a network key
+     *
+     * @param name friendly name
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the network key
+     *
+     * @return 16-byte network key
+     */
     public byte[] getKey() {
         return key;
     }
 
+    /**
+     * Returns the key refresh keyRefreshPhase of the network key
+     *
+     * @return int keyRefreshPhase
+     */
+    @KeyRefreshPhases
+    public int getKeyRefreshPhase() {
+        return keyRefreshPhase;
+    }
+
+    public void setKeyRefreshPhase(@KeyRefreshPhases final int keyRefreshPhase) {
+        this.keyRefreshPhase = keyRefreshPhase;
+    }
+
+    /**
+     * Returns the network key index
+     *
+     * @return key index
+     */
     public int getKeyIndex() {
         return keyIndex;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(final Parcel dest, final int flags) {
-        dest.writeInt(keyIndex);
-        dest.writeByteArray(key);
-    }
 }
