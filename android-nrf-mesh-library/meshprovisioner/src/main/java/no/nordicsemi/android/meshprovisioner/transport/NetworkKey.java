0f4eff7245c7ecc2d47d93d5497563940b340dce
Roshan Rajaratnam <roshan.rajaratnam@nordicsemi.no>
2018-12-12 09:10:08 +0100

    clean up

--- android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/NetworkKey.java
+++ android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/NetworkKey.java
@@ -3,7 +3,6 @@ package no.nordicsemi.android.meshprovisioner.transport;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
-import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 import android.os.Parcel;
