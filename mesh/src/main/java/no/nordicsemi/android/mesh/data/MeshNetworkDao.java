3cb55ae43166b03dcfac17bbd07beda1ef4b7177
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-02-09 11:22:54 +0100

    DB improvements to fix #392

--- mesh/src/main/java/no/nordicsemi/android/mesh/data/MeshNetworkDao.java
+++ mesh/src/main/java/no/nordicsemi/android/mesh/data/MeshNetworkDao.java
@@ -1,18 +1,16 @@
 package no.nordicsemi.android.mesh.data;
 
+import java.util.List;
+
+import androidx.annotation.RestrictTo;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
-import androidx.annotation.RestrictTo;
-
-import java.util.List;
-
 import no.nordicsemi.android.mesh.MeshNetwork;
 
-@SuppressWarnings("unused")
 @Dao
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 public interface MeshNetworkDao {
@@ -22,6 +20,11 @@ public interface MeshNetworkDao {
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
     void update(final MeshNetwork meshNetwork);
+
+    @Query("UPDATE mesh_network SET mesh_name = :meshName, timestamp = :timestamp, partial =:partial, " +
+            "iv_index =:ivIndex, last_selected =:lastSelected, networkExclusions =:networkExclusions WHERE mesh_uuid = :meshUUID")
+    void update(final String meshUUID, final String meshName, final long timestamp, final boolean partial,
+                final String ivIndex, final boolean lastSelected, final String networkExclusions);
 
     @Delete
     void delete(final MeshNetwork meshNetwork);
@@ -34,5 +37,4 @@ public interface MeshNetworkDao {
 
     @Query("SELECT * from mesh_network WHERE last_selected IS :lastSelected")
     MeshNetwork getMeshNetwork(final boolean lastSelected);
-
 }
