d57e3a2d5642e5f1faeeeabfc49b8370950ec7f9
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-02-11 14:47:58 +0100

    All status messages using an application key can be changed to type ApplicationStatusMessage

--- mesh/src/main/java/no/nordicsemi/android/mesh/transport/LightHslStatus.java
+++ mesh/src/main/java/no/nordicsemi/android/mesh/transport/LightHslStatus.java
@@ -38,7 +38,7 @@ import no.nordicsemi.android.mesh.utils.MeshParserUtils;
  * To be used as a wrapper class to create generic level status message.
  */
 @SuppressWarnings({"WeakerAccess"})
-public final class LightHslStatus extends GenericStatusMessage implements Parcelable {
+public final class LightHslStatus extends ApplicationStatusMessage implements Parcelable {
 
     private static final String TAG = LightHslStatus.class.getSimpleName();
     private static final int LIGHT_CTL_STATUS_MANDATORY_LENGTH = 6;
