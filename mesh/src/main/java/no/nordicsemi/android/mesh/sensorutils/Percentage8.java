5b283d5276537d9c09e0cd8c339f4d7ff8adf6a9
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-03-01 13:20:19 +0100

    Fix percentage parsing errors

--- mesh/src/main/java/no/nordicsemi/android/mesh/sensorutils/Percentage8.java
+++ mesh/src/main/java/no/nordicsemi/android/mesh/sensorutils/Percentage8.java
@@ -1,27 +1,35 @@
 package no.nordicsemi.android.mesh.sensorutils;
 
-import java.util.Locale;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+
+import static no.nordicsemi.android.mesh.utils.MeshParserUtils.unsignedToSigned;
 
 /**
  * The Percentage 8 characteristic is used to represent a measure of percentage.
  */
 public class Percentage8 extends DevicePropertyCharacteristic<Float> {
 
+    private static final String TAG = Percentage8.class.getSimpleName();
+
     public Percentage8(@NonNull final byte[] data, final int offset) {
         super(data, offset);
-        value = parse(data, offset, getLength(), 0,100, 0xFF);
+        value = unsignedToSigned(data[offset] & 0xFF, 8) / 2.0f;
+        if (value < 0.0f || value > 100.0f) {
+            this.value = null;
+            Log.e(TAG, "Value " + value + " is Prohibited!");
+        }
     }
 
-    public Percentage8(final float percentage){
+    public Percentage8(final float percentage) {
         value = percentage;
     }
 
     @NonNull
     @Override
     public String toString() {
-        return String.format(Locale.US, "%.1f", value);
+        return String.valueOf(value);
     }
 
     @Override
